82586aeb8779685fb4d5123c8baae65c6516290e
NNgocNhu84 <pth.pnhu2207@gmail.com>
2024-12-17 20:19:00 +0100

    Update MainActivity.kt

--- app/src/main/java/com/ngocnhungo/currency_calculator/MainActivity.kt
+++ app/src/main/java/com/ngocnhungo/currency_calculator/MainActivity.kt
@@ -2,34 +2,32 @@ package com.ngocnhungo.currency_calculator
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Spinner
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.ArrayAdapter
-
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "CutPasteId")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Tùy chỉnh giao diện với padding
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        // Kết nối Spinner và Adapter
         val spinner1: Spinner = findViewById(R.id.spinner1)
+        val spinner2: Spinner = findViewById(R.id.spinner2)
         val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.waehrung,
-            R.layout.spinner
-        )
-        val spinner2: Spinner = findViewById(R.id.spinner2 )
-        val adapter2 = ArrayAdapter.createFromResource(
             this,
             R.array.waehrung,
             R.layout.spinner
@@ -37,5 +35,50 @@ class MainActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(R.layout.spinner)
         spinner1.adapter = adapter
         spinner2.adapter = adapter
+
+        // Kết nối EditText
+        val editTextFrom: EditText = findViewById(R.id.editTextNumberDecimal4)
+        val editTextTo: EditText = findViewById(R.id.editTextNumberDecimal5)
+
+        // Hardcoded tỷ giá hối đoái
+        val exchangeRates = mapOf(
+            "EURtoVND" to 27663.0,
+            "EURtoUSD" to 1.09,
+            "USDtoEUR" to 0.92,
+            "VNDtoEUR" to 0.00003,
+            "USDtoVND" to 25422.0,
+            "VNDtoUSD" to 0.00004
+        )
+
+        // Lắng nghe sự thay đổi trong EditText "From"
+        editTextFrom.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val fromCurrency = spinner1.selectedItem.toString()
+                val toCurrency = spinner2.selectedItem.toString()
+
+                if (s.isNullOrEmpty() || fromCurrency == toCurrency) {
+                    editTextTo.setText("")
+                    return
+                }
+
+                val amount = s.toString().toDoubleOrNull()
+                if (amount != null) {
+                    val key = "${fromCurrency}to${toCurrency}"
+                    val rate = exchangeRates[key]
+
+                    if (rate != null) {
+                        val convertedAmount = amount * rate
+                        editTextTo.setText("%.6f".format(convertedAmount))
+                    } else {
+                        editTextTo.setText("Kein Kurs")
+                    }
+                } else {
+                    editTextTo.setText("")
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
